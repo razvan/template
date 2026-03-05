b6f6674954f1648179d1336d274638b911af1e93
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2026-03-05 10:54:37 +0100

    chore: add more templates

---
+++ lua/plugins/rust-analyzer.lua
@@ -0,0 +1,18 @@
+return {
+  "AstroNvim/astrolsp",
+  opts = {
+    config = {
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            -- This is needed because Stackable ops have a custom rustfmt.toml
+            -- that enables features that require nightly.
+            rustfmt = {
+              extraArgs = { "+nightly-2026-02-24" },
+            },
+          },
+        },
+      },
+    },
+  },
+}
