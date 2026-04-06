56e85338825e29717fa8ea55d01329563b8f048e
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2026-04-06 11:42:02 +0200

    chore: reenable snacks.gh

--- lua/plugins/snacks.gh.lua
+++ lua/plugins/snacks.gh.lua
@@ -1,7 +1,8 @@
 --
 -- Octo has better usability for editing issues.
+-- But Octo has more bugs too, so ...
 --
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 return {
   {
