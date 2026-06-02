4844e45c39ea13f0a36bf023917689b89a4453ab
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2026-06-02 14:44:50 +0200

    chore: re-enable some plugins

--- lua/community.lua
+++ lua/community.lua
@@ -9,12 +9,12 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
-  -- { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.rust" },
   -- { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.file-explorer.fyler-nvim" },
   -- Base pack with basedpyright and ruff
-  -- { import = "astrocommunity.pack.python.base" },
-  -- { import = "astrocommunity.pack.python.basedpyright" },
+  { import = "astrocommunity.pack.python.base" },
+  { import = "astrocommunity.pack.python.basedpyright" },
   -- { import = "astrocommunity.pack.python.ruff" },
   -- { import = "astrocommunity.pack.python.black" },
 }
